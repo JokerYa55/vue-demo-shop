abf9f884b0bad69dfcb1bb3dceed6f837602f138
vasiliy.andricov <vasiliy.andricov@south.rt.ru>
2021-08-18 16:36:09 +0300

    #

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
+class book {
+    isdn;
+    title;
+    pageCount = 0;
+    author;
+
+    constructor(isdn, title, pageCount, author) {
+        this.author = author;
+        this.isdn = isdn;
+        this.pageCount = pageCount;
+        this.title = title;
+    }
+}
+
 var main = new Vue({
     el: '#app',
     data: {
-        title : 'Книжный магазин'
+        title: 'Книжный магазин',
+        bookList = [new book('sdkjfhskdfskjd-1', 'Сказки', 100, 'Народ')]
     }
 });
